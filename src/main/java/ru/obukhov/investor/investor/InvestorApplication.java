5b476e4049a98a4aa0895c1a6d060dccf58f517b
auobukhov@gmail.com <auobukhov@gmail.com>
2020-08-08 15:56:15 +0300

    initial commit

---
+++ src/main/java/ru/obukhov/investor/investor/InvestorApplication.java
@@ -0,0 +1,13 @@
+package ru.obukhov.investor.investor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InvestorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InvestorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/obukhov/investor/investor/InvestorApplicationTests.java
@@ -0,0 +1,14 @@
+package ru.obukhov.investor.investor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InvestorApplicationTests {
+
+    @Test
+    @SuppressWarnings("squid:S2699")
+    void contextLoads() {
+    }
+
+}
