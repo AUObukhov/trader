d9225d4dca6e54f15d2a7dfb3b6b2af1f6dc3d3d
auobukhov@gmail.com <auobukhov@gmail.com>
2020-08-15 17:54:18 +0300

    moved scale and rounding mode from serializer to mapper

--- src/main/java/ru/obukhov/investor/model/Candle.java
+++ src/main/java/ru/obukhov/investor/model/Candle.java
@@ -1,9 +1,7 @@
 package ru.obukhov.investor.model;
 
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.Builder;
 import lombok.Data;
-import ru.obukhov.investor.model.transform.MoneySerializer;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -12,17 +10,11 @@ import java.time.OffsetDateTime;
 @Builder
 public class Candle {
 
-    @JsonSerialize(using = MoneySerializer.class)
     public BigDecimal openPrice;
-    @JsonSerialize(using = MoneySerializer.class)
     public BigDecimal closePrice;
-    @JsonSerialize(using = MoneySerializer.class)
     public BigDecimal saldo;
-    @JsonSerialize(using = MoneySerializer.class)
     public BigDecimal highestPrice;
-    @JsonSerialize(using = MoneySerializer.class)
     public BigDecimal lowestPrice;
-
     public OffsetDateTime time;
 
 }
--- src/main/java/ru/obukhov/investor/model/transform/CandleMapper.java
+++ src/main/java/ru/obukhov/investor/model/transform/CandleMapper.java
@@ -2,15 +2,33 @@ package ru.obukhov.investor.model.transform;
 
 import org.mapstruct.AfterMapping;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 import org.mapstruct.MappingTarget;
+import org.mapstruct.Mappings;
+import org.mapstruct.Named;
 import ru.obukhov.investor.model.Candle;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 
 @Mapper
 public abstract class CandleMapper {
+    @Mappings({
+            @Mapping(target = "openPrice", source = "openPrice", qualifiedByName = "moneyMapper"),
+            @Mapping(target = "closePrice", source = "closePrice", qualifiedByName = "moneyMapper"),
+            @Mapping(target = "highestPrice", source = "highestPrice", qualifiedByName = "moneyMapper"),
+            @Mapping(target = "lowestPrice", source = "lowestPrice", qualifiedByName = "moneyMapper"),
+    })
     public abstract Candle mapCandle(ru.tinkoff.invest.openapi.models.market.Candle source);
 
     @AfterMapping
-    protected void calculateSaldo(@MappingTarget Candle candle) {
-        candle.setSaldo(candle.getClosePrice().subtract(candle.getOpenPrice()));
+    protected void calculateSaldo(@MappingTarget Candle.CandleBuilder candle,
+                                  ru.tinkoff.invest.openapi.models.market.Candle source) {
+        candle.saldo(source.closePrice.subtract(source.openPrice).setScale(2, RoundingMode.HALF_UP));
+    }
+
+    @Named("moneyMapper")
+    protected BigDecimal mapMoney(BigDecimal source) {
+        return source.setScale(2, RoundingMode.HALF_UP);
     }
 }
