5a8b25c3d8632a630ec2fd9a67a755523bfecc07
auobukhov@gmail.com <auobukhov@gmail.com>
2020-09-20 16:48:53 +0300

    extended InvestorApplication from SpringBootServletInitializer for Tomcat

--- src/main/java/ru/obukhov/investor/InvestorApplication.java
+++ src/main/java/ru/obukhov/investor/InvestorApplication.java
@@ -2,17 +2,23 @@ package ru.obukhov.investor;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import ru.obukhov.investor.config.TradingProperties;
 
 @SpringBootApplication
 @EnableAspectJAutoProxy
 @EnableConfigurationProperties(TradingProperties.class)
-public class InvestorApplication {
+public class InvestorApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(InvestorApplication.class, args);
     }
 
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(InvestorApplication.class);
+    }
 }
