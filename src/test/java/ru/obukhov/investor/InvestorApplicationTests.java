4cd4a5076133258c4a817beb821a1fabb9459625
auobukhov@gmail.com <auobukhov@gmail.com>
2020-09-20 09:48:13 +0300

    fixed InvestorApplicationTests

--- src/test/java/ru/obukhov/investor/InvestorApplicationTests.java
+++ src/test/java/ru/obukhov/investor/InvestorApplicationTests.java
@@ -5,7 +5,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.obukhov.investor.service.interfaces.ConnectionService;
 
-@SpringBootTest
+@SpringBootTest(args = "i identify myself as token")
 class InvestorApplicationTests {
 
     @MockBean
