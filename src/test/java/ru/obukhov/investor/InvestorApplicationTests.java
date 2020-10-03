0d7cb14c12c53a02714ab1b6bdd3ac3465537956
auobukhov@gmail.com <auobukhov@gmail.com>
2020-10-03 10:15:32 +0300

    fixed context loading in test

--- src/test/java/ru/obukhov/investor/InvestorApplicationTests.java
+++ src/test/java/ru/obukhov/investor/InvestorApplicationTests.java
@@ -4,12 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import ru.obukhov.investor.service.interfaces.ConnectionService;
+import ru.tinkoff.invest.openapi.SandboxContext;
 
 @SpringBootTest(args = "i identify myself as token")
 class InvestorApplicationTests {
 
     @MockBean
     private ConnectionService connectionService;
+    @MockBean
+    private SandboxContext sandboxContext;
 
     @Test
     @SuppressWarnings("squid:S2699")
