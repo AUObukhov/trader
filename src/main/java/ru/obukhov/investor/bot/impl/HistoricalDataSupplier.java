4d5e921f713669b66c1a161d0308d27a5277ff4c
auobukhov@gmail.com <auobukhov@gmail.com>
2020-10-17 20:14:05 +0300

    fixed balance of position, return after simulation

--- src/main/java/ru/obukhov/investor/bot/impl/HistoricalDataSupplier.java
+++ src/main/java/ru/obukhov/investor/bot/impl/HistoricalDataSupplier.java
@@ -10,7 +10,9 @@ import ru.obukhov.investor.config.TradingProperties;
 import ru.obukhov.investor.model.Candle;
 import ru.obukhov.investor.service.interfaces.MarketService;
 import ru.obukhov.investor.util.DateUtils;
+import ru.obukhov.investor.util.MathUtils;
 import ru.tinkoff.invest.openapi.models.market.CandleInterval;
+import ru.tinkoff.invest.openapi.models.portfolio.Portfolio;
 
 import java.math.BigDecimal;
 import java.time.OffsetDateTime;
@@ -33,10 +35,11 @@ public class HistoricalDataSupplier implements DataSupplier {
     @Override
     public DecisionData getData(String ticker) {
 
+        BigDecimal currentPrice = getCurrentPrice(ticker);
         return DecisionData.builder()
                 .balance(marketMock.getBalance())
-                .position(marketMock.getPosition(ticker))
-                .currentPrice(getCurrentPrice(ticker))
+                .position(getPosition(ticker, currentPrice))
+                .currentPrice(currentPrice)
                 .lastOperations(Collections.emptyList())
                 .instrument(marketService.getInstrument(ticker))
                 .build();
@@ -51,6 +54,28 @@ public class HistoricalDataSupplier implements DataSupplier {
         updatePrices(ticker);
         return pricesHolder.getPrice(ticker, marketMock.getCurrentDateTime());
 
+    }
+
+    private Portfolio.PortfolioPosition getPosition(String ticker, BigDecimal currentPrice) {
+        Portfolio.PortfolioPosition position = marketMock.getPosition(ticker);
+        if (position == null) {
+            return null;
+        }
+
+        BigDecimal balance = MathUtils.multiply(currentPrice, position.lots);
+
+        return new Portfolio.PortfolioPosition(
+                position.figi,
+                position.ticker,
+                position.isin,
+                position.instrumentType,
+                balance,
+                position.blocked,
+                position.expectedYield,
+                position.lots,
+                position.averagePositionPrice,
+                position.averagePositionPriceNoNkd,
+                position.name);
     }
 
     private void updatePrices(String ticker) {
