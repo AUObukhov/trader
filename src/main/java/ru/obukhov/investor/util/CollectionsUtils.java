2654d7d2208244d95f365b8dd6cedd5bf2e46dca
auobukhov@gmail.com <auobukhov@gmail.com>
2021-01-10 15:42:02 +0300

    added CollectionsUtils#insertInterpolatedElement

--- src/main/java/ru/obukhov/investor/util/CollectionsUtils.java
+++ src/main/java/ru/obukhov/investor/util/CollectionsUtils.java
@@ -1,13 +1,16 @@
 package ru.obukhov.investor.util;
 
+import com.google.common.collect.Iterables;
 import com.google.common.collect.Multimap;
 import lombok.AccessLevel;
 import lombok.NoArgsConstructor;
+import org.springframework.util.Assert;
 
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.function.BinaryOperator;
 import java.util.function.Function;
 
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
@@ -43,6 +46,31 @@ public class CollectionsUtils {
         }
 
         return list.subList(list.size() - size, list.size());
+    }
+
+    /**
+     * Interpolates element at given {@code index} in given {@code list} and inserts it there.<br/>
+     * If {@code index} = 0, then interpolated element is equal to first element of {@code list}.<br/>
+     * If {@code index} = {@code list.size()} then interpolated element is equal to last element of {@code list}.<br/>
+     * Otherwise interpolated element is computed by given {@code interpolator} from
+     * {@code list.get(index - 1)} and {@code list.get(index)}
+     *
+     * @throws IllegalArgumentException {@code index} is negative or greater then size of list
+     * @throws IllegalArgumentException {@code list} is empty
+     */
+    public static <T> void insertInterpolated(List<T> list, int index, BinaryOperator<T> interpolator) {
+        Assert.isTrue(index >= 0, "index can't be negative");
+        Assert.isTrue(index <= list.size(), "index can't be greater than size of list");
+        Assert.isTrue(!list.isEmpty(), "list can't be empty");
+
+        if (index == 0) {
+            list.add(0, list.get(0));
+        } else if (index == list.size()) {
+            list.add(Iterables.getLast(list));
+        } else {
+            T value = interpolator.apply(list.get(index - 1), list.get(index));
+            list.add(index, value);
+        }
     }
 
 }
--- src/test/java/ru/obukhov/investor/util/CollectionsUtilsTest.java
+++ src/test/java/ru/obukhov/investor/util/CollectionsUtilsTest.java
@@ -5,10 +5,12 @@ import com.google.common.collect.MultimapBuilder;
 import org.junit.Test;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
 
+import static com.google.common.collect.Lists.newArrayList;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static ru.obukhov.investor.util.MathUtils.numbersEqual;
@@ -77,6 +79,74 @@ public class CollectionsUtilsTest {
         for (int i = 0; i < list.size(); i++) {
             assertEquals(list.get(0), result.get(0));
         }
+    }
+
+    // endregion
+
+    // region insertInterpolated tests
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertInterpolated_throwsIllegalArgumentException_whenIndexIsNegative() {
+        List<BigDecimal> list = Arrays.asList(BigDecimal.ONE, BigDecimal.TEN);
+        int index = -1;
+
+        CollectionsUtils.insertInterpolated(list, index, MathUtils::getAverage);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertInterpolated_throwsIllegalArgumentException_whenIndexIsGreaterThanListSize() {
+        List<BigDecimal> list = Arrays.asList(BigDecimal.ONE, BigDecimal.TEN);
+        int index = 3;
+
+        CollectionsUtils.insertInterpolated(list, index, MathUtils::getAverage);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void insertInterpolated_throwsIllegalArgumentException_whenListIsEmpty() {
+        List<BigDecimal> list = new ArrayList<>();
+        int index = 0;
+
+        CollectionsUtils.insertInterpolated(list, index, MathUtils::getAverage);
+    }
+
+    @Test
+    public void insertInterpolated_addsElementToBeginning_whenIndexIsZero() {
+        List<BigDecimal> list = newArrayList(BigDecimal.valueOf(5), BigDecimal.TEN);
+        int index = 0;
+
+        CollectionsUtils.insertInterpolated(list, index, MathUtils::getAverage);
+
+        assertEquals(3, list.size());
+        assertTrue(MathUtils.numbersEqual(list.get(0), 5));
+        assertTrue(MathUtils.numbersEqual(list.get(1), 5));
+        assertTrue(MathUtils.numbersEqual(list.get(2), 10));
+    }
+
+    @Test
+    public void insertInterpolated_addsElementToEnd_whenIndexIsEqualsToListSize() {
+        List<BigDecimal> list = newArrayList(BigDecimal.valueOf(5), BigDecimal.TEN);
+        int index = 2;
+
+        CollectionsUtils.insertInterpolated(list, index, MathUtils::getAverage);
+
+        assertEquals(3, list.size());
+        assertTrue(MathUtils.numbersEqual(list.get(0), 5));
+        assertTrue(MathUtils.numbersEqual(list.get(1), 10));
+        assertTrue(MathUtils.numbersEqual(list.get(2), 10));
+    }
+
+    @Test
+    public void insertInterpolated_addsElementToMiddle_whenIndexIsInMiddle() {
+        List<BigDecimal> list = newArrayList(BigDecimal.valueOf(5), BigDecimal.TEN, BigDecimal.ZERO);
+        int index = 1;
+
+        CollectionsUtils.insertInterpolated(list, index, MathUtils::getAverage);
+
+        assertEquals(4, list.size());
+        assertTrue(MathUtils.numbersEqual(list.get(0), 5));
+        assertTrue(MathUtils.numbersEqual(list.get(1), 7.5));
+        assertTrue(MathUtils.numbersEqual(list.get(2), 10));
+        assertTrue(MathUtils.numbersEqual(list.get(3), 0));
     }
 
     // endregion
