15ab974953a0f62c938698558550ccc58a3b952c
auobukhov@gmail.com <auobukhov@gmail.com>
2021-01-24 13:01:13 +0300

    removed redundant cache

--- src/main/java/ru/obukhov/investor/service/impl/MarketServiceImpl.java
+++ src/main/java/ru/obukhov/investor/service/impl/MarketServiceImpl.java
@@ -3,7 +3,6 @@ package ru.obukhov.investor.service.impl;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang3.ObjectUtils;
-import org.springframework.cache.annotation.Cacheable;
 import org.springframework.util.Assert;
 import org.springframework.util.CollectionUtils;
 import ru.obukhov.investor.bot.interfaces.TinkoffService;
@@ -40,7 +39,6 @@ public class MarketServiceImpl implements MarketService {
      * @return sorted by time list of loaded candles
      */
     @Override
-    @Cacheable("candles")
     public List<Candle> getCandles(String ticker, Interval interval, CandleInterval candleInterval) {
 
         Assert.isTrue(interval.getTo() == null
