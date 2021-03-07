1811fc65865f19f8c10a1700c6510981dd6f2f62
auobukhov@gmail.com <auobukhov@gmail.com>
2021-03-07 17:58:47 +0300

    removed OrdersController#placeOrder

--- src/main/java/ru/obukhov/investor/web/controller/OrdersController.java
+++ src/main/java/ru/obukhov/investor/web/controller/OrdersController.java
@@ -3,17 +3,12 @@ package ru.obukhov.investor.web.controller;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import ru.obukhov.investor.service.interfaces.OrdersService;
 import ru.obukhov.investor.web.model.GetOrdersResponse;
-import ru.obukhov.investor.web.model.PlaceOrderRequest;
-import ru.obukhov.investor.web.model.PlaceOrderResponse;
 import ru.tinkoff.invest.openapi.models.orders.Order;
-import ru.tinkoff.invest.openapi.models.orders.PlacedOrder;
 
-import javax.validation.Valid;
 import java.util.List;
 
 @Slf4j
@@ -33,18 +28,6 @@ public class OrdersController {
         List<Order> orders = ordersService.getOrders();
 
         return new GetOrdersResponse(orders);
-
-    }
-
-    @GetMapping("/place")
-    public PlaceOrderResponse placeOrder(@Valid @RequestBody PlaceOrderRequest request) {
-
-        PlacedOrder placedOrder = ordersService.placeOrder(request.getTicker(),
-                request.getLots(),
-                request.getOperation(),
-                request.getPrice());
-
-        return new PlaceOrderResponse(placedOrder);
 
     }
 
