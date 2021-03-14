5d4ecfeecaee128191faa507a38f50a6b786f8fb
auobukhov@gmail.com <auobukhov@gmail.com>
2021-03-14 15:50:12 +0300

    replaced @EnableConfigurationProperties by @ConfigurationPropertiesScan

--- src/main/java/ru/obukhov/trader/InvestorApplication.java
+++ src/main/java/ru/obukhov/trader/InvestorApplication.java
@@ -3,19 +3,17 @@ package ru.obukhov.trader;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
-import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
-import ru.obukhov.trader.config.BotConfig;
-import ru.obukhov.trader.config.TradingProperties;
 
 @SpringBootApplication
 @EnableCaching
 @EnableScheduling
 @EnableAspectJAutoProxy
-@EnableConfigurationProperties({TradingProperties.class, BotConfig.class})
+@ConfigurationPropertiesScan
 public class InvestorApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
