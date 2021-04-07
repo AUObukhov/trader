0aacb0319c6b51e0097c45734fae04dfef2a53ac
auobukhov@gmail.com <auobukhov@gmail.com>
2021-04-07 20:47:07 +0300

    added BigDecimalSerializerUnitTest

---
+++ src/test/java/ru/obukhov/trader/common/model/transform/BigDecimalSerializerUnitTest.java
@@ -0,0 +1,52 @@
+package ru.obukhov.trader.common.model.transform;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import org.mockito.ArgumentCaptor;
+import org.mockito.Captor;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import ru.obukhov.trader.BaseMockedTest;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+class BigDecimalSerializerUnitTest extends BaseMockedTest {
+
+    private final BigDecimalSerializer serializer = new BigDecimalSerializer();
+
+    @Mock
+    private JsonGenerator generator;
+    @Captor
+    private ArgumentCaptor<BigDecimal> argumentCaptor;
+
+    @Test
+    void serialize_passesNull_whenValueIsNull() throws IOException {
+        BigDecimal value = null;
+
+        serializer.serialize(value, generator, null);
+
+        Mockito.verify(generator, Mockito.times(1)).writeNumber(argumentCaptor.capture());
+
+        Assertions.assertNull(argumentCaptor.getValue());
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            ",",
+            "100, 100",
+            "100.12345, 100.12345",
+            "100.123454, 100.12345",
+            "100.123456, 100.12346",
+    })
+    void serialize(BigDecimal value, BigDecimal expectedPassedValue) throws IOException {
+        serializer.serialize(value, generator, null);
+
+        Mockito.verify(generator, Mockito.times(1))
+                .writeNumber(expectedPassedValue);
+    }
+
+}
