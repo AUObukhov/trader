cd3c65235e0b118a9e8a4974252273267ca140e7
auobukhov@gmail.com <auobukhov@gmail.com>
2021-04-10 12:20:46 +0300

    added TraderExceptionHandlerIntegrationTest

---
+++ src/test/java/ru/obukhov/trader/web/TraderExceptionHandlerIntegrationTest.java
@@ -0,0 +1,106 @@
+package ru.obukhov.trader.web;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.StringUtils;
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.mockito.MockedStatic;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.core.MethodParameter;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.validation.BeanPropertyBindingResult;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.obukhov.trader.Application;
+import ru.obukhov.trader.common.util.DateUtils;
+import ru.obukhov.trader.test.utils.ResourceUtils;
+import ru.obukhov.trader.test.utils.TestDataHelper;
+
+import java.lang.reflect.Method;
+import java.time.OffsetDateTime;
+
+@RunWith(SpringRunner.class)
+@AutoConfigureMockMvc
+@SpringBootTest(
+        classes = Application.class,
+        webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
+        args = "--trading.token=i identify myself as token"
+)
+class TraderExceptionHandlerIntegrationTest {
+
+    private final TestController controller = new TestController();
+    private final MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
+            .setControllerAdvice(new TraderExceptionHandler())
+            .build();
+
+    @Test
+    @SuppressWarnings("unused")
+    void handlesMethodArgumentNotValidException() throws Exception {
+        OffsetDateTime mockedNow =
+                DateUtils.getDateTime(2020, 9, 23, 10, 0, 0);
+        try (MockedStatic<OffsetDateTime> offsetDateTimeStaticMock = TestDataHelper.mockNow(mockedNow)) {
+            String expectedResponse = ResourceUtils.getResourceAsString("test-data/TestValidationResponse.json");
+
+            mockMvc.perform(MockMvcRequestBuilders.post("/trader/test/validation")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().isBadRequest())
+                    .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.content().json(expectedResponse));
+        }
+    }
+
+    @Test
+    @SuppressWarnings("unused")
+    void handlesRuntimeException() throws Exception {
+        OffsetDateTime mockedNow =
+                DateUtils.getDateTime(2020, 9, 23, 10, 0, 0);
+        try (MockedStatic<OffsetDateTime> offsetDateTimeStaticMock = TestDataHelper.mockNow(mockedNow)) {
+            String expectedResponse = ResourceUtils.getResourceAsString("test-data/RuntimeExceptionResponse.json");
+
+            mockMvc.perform(MockMvcRequestBuilders.post("/trader/test/runtime")
+                    .contentType(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.status().isInternalServerError())
+                    .andExpect(MockMvcResultMatchers.content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                    .andExpect(MockMvcResultMatchers.content().json(expectedResponse));
+        }
+    }
+
+    @Slf4j
+    @RestController
+    @RequestMapping("trader/test")
+    @RequiredArgsConstructor
+    @SuppressWarnings("unused")
+    public static class TestController {
+
+        @PostMapping("/validation")
+        public void throwMethodArgumentNotValidException()
+                throws MethodArgumentNotValidException, NoSuchMethodException {
+
+            Method method = this.getClass().getMethod("throwMethodArgumentNotValidException");
+            MethodParameter parameter = new MethodParameter(method, -1);
+
+            BindingResult bindingResult = new BeanPropertyBindingResult(null, StringUtils.EMPTY);
+            bindingResult.addError(new ObjectError("objectName1", "validation error1"));
+            bindingResult.addError(new ObjectError("objectName2", "validation error2"));
+
+            throw new MethodArgumentNotValidException(parameter, bindingResult);
+        }
+
+        @PostMapping("/runtime")
+        public void throwRuntimeException() {
+            throw new RuntimeException("runtime exception message");
+        }
+
+    }
+}
