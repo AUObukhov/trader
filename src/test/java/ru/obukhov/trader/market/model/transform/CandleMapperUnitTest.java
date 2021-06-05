41abc0de41cb24726063439934f133eb9337a919
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 11:27:57 +0300

    refactored CandleMapperUnitTest

--- src/test/java/ru/obukhov/trader/market/model/transform/CandleMapperUnitTest.java
+++ src/test/java/ru/obukhov/trader/market/model/transform/CandleMapperUnitTest.java
@@ -18,67 +18,61 @@ class CandleMapperUnitTest {
 
     @Test
     void mapsSingleCandleFields() {
-
-        ru.tinkoff.invest.openapi.model.rest.Candle source = TestDataHelper.createTinkoffCandle(
+        final ru.tinkoff.invest.openapi.model.rest.Candle source = TestDataHelper.createTinkoffCandle(
                 100, 200, 1000, 50);
 
-        Candle result = candleMapper.map(source);
+        final Candle result = candleMapper.map(source);
 
         AssertUtils.assertEquals(source.getO(), result.getOpenPrice());
         AssertUtils.assertEquals(source.getC(), result.getClosePrice());
         AssertUtils.assertEquals(source.getH(), result.getHighestPrice());
         AssertUtils.assertEquals(source.getL(), result.getLowestPrice());
         Assertions.assertEquals(source.getTime(), result.getTime());
-
     }
 
     @Test
     void updatesOffset() {
-
-        ru.tinkoff.invest.openapi.model.rest.Candle source = TestDataHelper.createTinkoffCandle(
+        final ru.tinkoff.invest.openapi.model.rest.Candle source = TestDataHelper.createTinkoffCandle(
                 CandleResolution.DAY, 0, 0, 0, 0
         );
 
-        Candle result = candleMapper.map(source);
+        final Candle result = candleMapper.map(source);
 
         Assertions.assertEquals(source.getTime(), result.getTime());
-
     }
 
     @Test
     void mapsMultipleCandles() {
-
-        ru.tinkoff.invest.openapi.model.rest.Candle tinkoffCandle1 = TestDataHelper.createTinkoffCandle(
+        final ru.tinkoff.invest.openapi.model.rest.Candle tinkoffCandle1 = TestDataHelper.createTinkoffCandle(
                 100, 200, 1000, 50
         );
 
-        ru.tinkoff.invest.openapi.model.rest.Candle tinkoffCandle2 = TestDataHelper.createTinkoffCandle(
+        final ru.tinkoff.invest.openapi.model.rest.Candle tinkoffCandle2 = TestDataHelper.createTinkoffCandle(
                 200, 400, 2000, 100
         );
 
-        List<ru.tinkoff.invest.openapi.model.rest.Candle> candles = List.of(tinkoffCandle1, tinkoffCandle2);
-        Candles source = new Candles();
+        final List<ru.tinkoff.invest.openapi.model.rest.Candle> candles = List.of(tinkoffCandle1, tinkoffCandle2);
+        final Candles source = new Candles();
         source.setFigi(StringUtils.EMPTY);
         source.setInterval(CandleResolution.DAY);
         source.setCandles(candles);
 
-        List<Candle> result = candleMapper.map(source);
+        final List<Candle> result = candleMapper.map(source);
         Assertions.assertEquals(2, result.size());
 
-        Candle candle1 = result.get(0);
+        final Candle candle1 = result.get(0);
         AssertUtils.assertEquals(tinkoffCandle1.getO(), candle1.getOpenPrice());
         AssertUtils.assertEquals(tinkoffCandle1.getC(), candle1.getClosePrice());
         AssertUtils.assertEquals(tinkoffCandle1.getH(), candle1.getHighestPrice());
         AssertUtils.assertEquals(tinkoffCandle1.getL(), candle1.getLowestPrice());
         Assertions.assertEquals(tinkoffCandle1.getTime(), candle1.getTime());
 
-        Candle candle2 = result.get(1);
+        final Candle candle2 = result.get(1);
         AssertUtils.assertEquals(tinkoffCandle2.getO(), candle2.getOpenPrice());
         AssertUtils.assertEquals(tinkoffCandle2.getC(), candle2.getClosePrice());
         AssertUtils.assertEquals(tinkoffCandle2.getH(), candle2.getHighestPrice());
         AssertUtils.assertEquals(tinkoffCandle2.getL(), candle2.getLowestPrice());
         Assertions.assertEquals(tinkoffCandle2.getTime(), candle2.getTime());
-
     }
 
 }
