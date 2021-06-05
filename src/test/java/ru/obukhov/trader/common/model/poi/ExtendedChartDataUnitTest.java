5c9b0a02e110552529c597b5d6800121d04cbcdf
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 10:39:17 +0300

    refactored ExtendedChartDataUnitTest

--- src/test/java/ru/obukhov/trader/common/model/poi/ExtendedChartDataUnitTest.java
+++ src/test/java/ru/obukhov/trader/common/model/poi/ExtendedChartDataUnitTest.java
@@ -8,6 +8,7 @@ import org.apache.poi.xddf.usermodel.chart.XDDFLineChartData;
 import org.apache.poi.xddf.usermodel.chart.XDDFNumericalDataSource;
 import org.apache.poi.xddf.usermodel.chart.XDDFValueAxis;
 import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ru.obukhov.trader.test.utils.AssertUtils;
 
@@ -16,10 +17,15 @@ import java.util.List;
 
 class ExtendedChartDataUnitTest {
 
+    private ExtendedChartData chartData;
+
+    @BeforeEach
+    void setUp() {
+        chartData = ExcelTestDataHelper.createExtendedChartData();
+    }
+
     @Test
     void addSeries_withColor_andGetSeries() {
-        final ExtendedChartData chartData = ExcelTestDataHelper.createExtendedChartData();
-
         final String[] category1 = new String[]{"s1", "s2", "s3"};
         final Integer[] values1 = new Integer[]{1, 2, 3};
         final MarkerProperties markerProperties1 = new MarkerProperties((short) 2, MarkerStyle.CIRCLE, Color.GREEN);
@@ -56,8 +62,6 @@ class ExtendedChartDataUnitTest {
 
     @Test
     void addSeries_withoutColors_andGetSeries() {
-        ExtendedChartData chartData = ExcelTestDataHelper.createExtendedChartData();
-
         final String[] category1 = new String[]{"s1", "s2", "s3"};
         final Integer[] values1 = new Integer[]{1, 2, 3};
         final MarkerProperties markerProperties1 = new MarkerProperties((short) 2, MarkerStyle.CIRCLE, null);
@@ -74,15 +78,15 @@ class ExtendedChartDataUnitTest {
         final XDDFNumericalDataSource<Number> numericalDataSource2 = XDDFDataSourcesFactory.fromArray(values2);
         chartData.addSeries(categoryDataSource2, numericalDataSource2, markerProperties2);
 
-        List<XDDFChartData.Series> seriesList = chartData.getSeries();
+        final List<XDDFChartData.Series> seriesList = chartData.getSeries();
         Assertions.assertEquals(2, seriesList.size());
 
-        XDDFLineChartData.Series series1 = (XDDFLineChartData.Series) seriesList.get(0);
+        final XDDFLineChartData.Series series1 = (XDDFLineChartData.Series) seriesList.get(0);
         Assertions.assertSame(categoryDataSource1, series1.getCategoryData());
         Assertions.assertSame(numericalDataSource1, series1.getValuesData());
         Assertions.assertNull(series1.getShapeProperties());
 
-        XDDFLineChartData.Series series2 = (XDDFLineChartData.Series) seriesList.get(1);
+        final XDDFLineChartData.Series series2 = (XDDFLineChartData.Series) seriesList.get(1);
         Assertions.assertSame(categoryDataSource2, series2.getCategoryData());
         Assertions.assertSame(numericalDataSource2, series2.getValuesData());
         Assertions.assertNull(series2.getShapeProperties());
@@ -92,57 +96,51 @@ class ExtendedChartDataUnitTest {
 
     @Test
     void stretchChart_doesNothing_whenNoValues() {
-        ExtendedChartData chartData = ExcelTestDataHelper.createExtendedChartData();
-
-        String[] category = new String[0];
-        Integer[] values = new Integer[0];
-        XDDFCategoryDataSource categoryDataSource = XDDFDataSourcesFactory.fromArray(category);
-        XDDFNumericalDataSource<Number> numericalDataSource = XDDFDataSourcesFactory.fromArray(values);
+        final String[] category = new String[0];
+        final Integer[] values = new Integer[0];
+        final XDDFCategoryDataSource categoryDataSource = XDDFDataSourcesFactory.fromArray(category);
+        final XDDFNumericalDataSource<Number> numericalDataSource = XDDFDataSourcesFactory.fromArray(values);
         final MarkerProperties markerProperties = new MarkerProperties((short) 2, MarkerStyle.CIRCLE, Color.BLUE);
 
         chartData.addSeries(categoryDataSource, numericalDataSource, markerProperties, Color.RED);
 
         chartData.stretchChart();
 
-        XDDFValueAxis valueAxis = chartData.getValueAxes().get(0);
+        final XDDFValueAxis valueAxis = chartData.getValueAxes().get(0);
         AssertUtils.assertEquals(Double.NaN, valueAxis.getMinimum());
         AssertUtils.assertEquals(Double.NaN, valueAxis.getMaximum());
     }
 
     @Test
     void stretchChart_doesNothing_whenAllValuesAreNull() {
-        ExtendedChartData chartData = ExcelTestDataHelper.createExtendedChartData();
-
-        String[] category = new String[]{"s1", "s2", "s3"};
-        Integer[] values = new Integer[]{null, null, null};
-        XDDFCategoryDataSource categoryDataSource = XDDFDataSourcesFactory.fromArray(category);
-        XDDFNumericalDataSource<Number> numericalDataSource = XDDFDataSourcesFactory.fromArray(values);
+        final String[] category = new String[]{"s1", "s2", "s3"};
+        final Integer[] values = new Integer[]{null, null, null};
+        final XDDFCategoryDataSource categoryDataSource = XDDFDataSourcesFactory.fromArray(category);
+        final XDDFNumericalDataSource<Number> numericalDataSource = XDDFDataSourcesFactory.fromArray(values);
         final MarkerProperties markerProperties = new MarkerProperties((short) 2, MarkerStyle.CIRCLE, Color.BLUE);
 
         chartData.addSeries(categoryDataSource, numericalDataSource, markerProperties, Color.RED);
 
         chartData.stretchChart();
 
-        XDDFValueAxis valueAxis = chartData.getValueAxes().get(0);
+        final XDDFValueAxis valueAxis = chartData.getValueAxes().get(0);
         AssertUtils.assertEquals(Double.NaN, valueAxis.getMinimum());
         AssertUtils.assertEquals(Double.NaN, valueAxis.getMaximum());
     }
 
     @Test
     void stretchChart_stretchesChart_whenValuesAreNotEmpty() {
-        ExtendedChartData chartData = ExcelTestDataHelper.createExtendedChartData();
-
-        String[] category = new String[]{"s1", "s2", "s3", "s4", "s5"};
-        Integer[] values = new Integer[]{1, null, 3, 2, 0};
-        XDDFCategoryDataSource categoryDataSource = XDDFDataSourcesFactory.fromArray(category);
-        XDDFNumericalDataSource<Number> numericalDataSource = XDDFDataSourcesFactory.fromArray(values);
+        final String[] category = new String[]{"s1", "s2", "s3", "s4", "s5"};
+        final Integer[] values = new Integer[]{1, null, 3, 2, 0};
+        final XDDFCategoryDataSource categoryDataSource = XDDFDataSourcesFactory.fromArray(category);
+        final XDDFNumericalDataSource<Number> numericalDataSource = XDDFDataSourcesFactory.fromArray(values);
         final MarkerProperties markerProperties = new MarkerProperties((short) 2, MarkerStyle.CIRCLE, Color.BLUE);
 
         chartData.addSeries(categoryDataSource, numericalDataSource, markerProperties, Color.RED);
 
         chartData.stretchChart();
 
-        XDDFValueAxis valueAxis = chartData.getValueAxes().get(0);
+        final XDDFValueAxis valueAxis = chartData.getValueAxes().get(0);
         AssertUtils.assertEquals(0, valueAxis.getMinimum());
         AssertUtils.assertEquals(3, valueAxis.getMaximum());
     }
