ea26d74231ce2a07dc537f79e8631e42559afc26
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 10:47:40 +0300

    refactored OffsetTimeConverterUnitTest

--- src/test/java/ru/obukhov/trader/common/model/transform/OffsetTimeConverterUnitTest.java
+++ src/test/java/ru/obukhov/trader/common/model/transform/OffsetTimeConverterUnitTest.java
@@ -12,9 +12,10 @@ class OffsetTimeConverterUnitTest {
 
     @Test
     void convert() {
-        OffsetTime offsetTime = converter.convert("10:15:30.000005+01:00");
+        final OffsetTime offsetTime = converter.convert("10:15:30.000005+01:00");
 
-        OffsetTime expectedTime = OffsetTime.of(10, 15, 30, 5000, ZoneOffset.ofHours(1));
+        final OffsetTime expectedTime =
+                OffsetTime.of(10, 15, 30, 5000, ZoneOffset.ofHours(1));
 
         Assertions.assertEquals(expectedTime, offsetTime);
     }
