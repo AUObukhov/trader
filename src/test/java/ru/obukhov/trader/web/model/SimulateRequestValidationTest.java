909ddd2bff1b04b9ce129fcb40b89d0b44423c94
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 11:50:13 +0300

    refactored SimulateRequestValidationTest

--- src/test/java/ru/obukhov/trader/web/model/SimulateRequestValidationTest.java
+++ src/test/java/ru/obukhov/trader/web/model/SimulateRequestValidationTest.java
@@ -2,7 +2,6 @@ package ru.obukhov.trader.web.model;
 
 import org.apache.commons.lang3.StringUtils;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.quartz.CronExpression;
 import ru.obukhov.trader.bot.model.StrategyConfig;
@@ -22,18 +21,13 @@ import java.util.Set;
 
 class SimulateRequestValidationTest {
 
-    private Validator validator;
-
-    @BeforeEach
-    public void setUp() {
-        validator = Validation.buildDefaultValidatorFactory().getValidator();
-    }
+    private final Validator validator = Validation.buildDefaultValidatorFactory().getValidator();
 
     @Test
     void validationSucceeds_whenEverythingIsValid() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         Assertions.assertTrue(violations.isEmpty());
     }
 
@@ -41,28 +35,28 @@ class SimulateRequestValidationTest {
 
     @Test
     void validationFails_whenTickerIsNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setTicker(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "ticker is mandatory");
     }
 
     @Test
     void validationFails_whenTickerIsEmpty() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setTicker(StringUtils.EMPTY);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "ticker is mandatory");
     }
 
     @Test
     void validationFails_whenTickerIsBlank() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setTicker("     ");
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "ticker is mandatory");
     }
 
@@ -70,39 +64,39 @@ class SimulateRequestValidationTest {
 
     @Test
     void validationFails_whenInitialBalanceIsNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setInitialBalance(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "initial balance is mandatory");
     }
 
     @Test
     void validationFails_whenBalanceIncrementIsNullAndBalanceIncrementCronIsNotNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setBalanceIncrement(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations,
                 "balanceIncrement and balanceIncrementCron must be both null or not null");
     }
 
     @Test
     void validationFails_whenBalanceIncrementIsNotNullAndBalanceIncrementCronIsNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setBalanceIncrementCron(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations,
                 "balanceIncrement and balanceIncrementCron must be both null or not null");
     }
 
     @Test
     void validationFails_whenFromIsNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setFrom(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "from is mandatory");
     }
 
@@ -110,35 +104,35 @@ class SimulateRequestValidationTest {
 
     @Test
     void validationFails_whenStrategiesConfigsIsNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setStrategiesConfigs(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "strategiesConfigs is mandatory");
     }
 
     @Test
     void validationFails_whenStrategiesConfigsIsEmpty() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.setStrategiesConfigs(Collections.emptyList());
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "strategiesConfigs is mandatory");
     }
 
     @Test
     void validationFails_whenStrategyTypeIsNull() throws ParseException {
-        SimulateRequest request = createValidSimulationRequest();
+        final SimulateRequest request = createValidSimulationRequest();
         request.getStrategiesConfigs().get(0).setType(null);
 
-        Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
+        final Set<ConstraintViolation<SimulateRequest>> violations = validator.validate(request);
         AssertUtils.assertViolation(violations, "type in StrategyConfig is mandatory");
     }
 
     // endregion
 
     private SimulateRequest createValidSimulationRequest() throws ParseException {
-        SimulateRequest request = new SimulateRequest();
+        final SimulateRequest request = new SimulateRequest();
 
         request.setTicker("ticker");
 
@@ -148,7 +142,7 @@ class SimulateRequestValidationTest {
 
         request.setFrom(OffsetDateTime.now());
 
-        StrategyConfig strategyConfig = new StrategyConfig(StrategyType.CONSERVATIVE);
+        final StrategyConfig strategyConfig = new StrategyConfig(StrategyType.CONSERVATIVE);
         request.setStrategiesConfigs(List.of(strategyConfig));
 
         return request;
