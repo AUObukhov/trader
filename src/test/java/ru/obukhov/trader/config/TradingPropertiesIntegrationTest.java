58b417255b74eac9eb2b39dc40564a63fb214c96
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 11:21:20 +0300

    refactored TradingPropertiesIntegrationTest

--- src/test/java/ru/obukhov/trader/config/TradingPropertiesIntegrationTest.java
+++ src/test/java/ru/obukhov/trader/config/TradingPropertiesIntegrationTest.java
@@ -29,110 +29,106 @@ class TradingPropertiesIntegrationTest {
                 .run(context -> {
                     Assertions.assertNull(context.getStartupFailure());
 
-                    TradingProperties tradingProperties = context.getBean(TradingProperties.class);
+                    final TradingProperties tradingProperties = context.getBean(TradingProperties.class);
 
                     AssertUtils.assertEquals(0.1d, tradingProperties.getCommission());
 
-                    OffsetTime expectedWorkStartTime = DateUtils.getTime(12, 0, 0).toOffsetTime();
+                    final OffsetTime expectedWorkStartTime = DateUtils.getTime(12, 0, 0).toOffsetTime();
                     Assertions.assertEquals(expectedWorkStartTime, tradingProperties.getWorkStartTime());
 
                     Assertions.assertEquals(Duration.ofMinutes(480), tradingProperties.getWorkDuration());
 
                     Assertions.assertEquals(Integer.valueOf(5), tradingProperties.getConsecutiveEmptyDaysLimit());
                 });
-
     }
 
     @Test
     void beanCreationFails_whenTokenIsNull() {
-
         this.contextRunner
-                .run(context -> assertContextStartupFailed(context,
-                        "trading.token", "не должно быть пустым")
-                );
-
+                .run(context -> assertContextStartupFailed(
+                        context,
+                        "trading.token", "не должно быть пустым"
+                ));
     }
 
     @Test
     void beanCreationFails_whenTokenIsBlank() {
-
         this.contextRunner
                 .withPropertyValues("trading.token=")
-                .run(context -> assertContextStartupFailed(context,
-                        "trading.token", "не должно быть пустым")
-                );
-
+                .run(context -> assertContextStartupFailed(
+                        context,
+                        "trading.token", "не должно быть пустым"
+                ));
     }
 
     @Test
     void beanCreationFails_whenWorkStartTimeIsNull() {
-
         this.contextRunner
                 .withPropertyValues("trading.token: i identify myself as token")
                 .withPropertyValues("trading.work-duration: 480")
                 .withPropertyValues("trading.consecutive-empty-days-limit: 5")
                 .withPropertyValues("start-date: 2000-01-01T00:00:00+03:00")
-                .run(context -> assertContextStartupFailed(context,
-                        "trading.workStartTime", "не должно равняться null")
-                );
-
+                .run(context -> assertContextStartupFailed(
+                        context,
+                        "trading.workStartTime", "не должно равняться null"
+                ));
     }
 
     @Test
     void beanCreationFails_whenWorkDurationIsNull() {
-
         this.contextRunner
                 .withPropertyValues("trading.token: i identify myself as token")
                 .withPropertyValues("trading.work-start-time: 12:00:00+03:00")
                 .withPropertyValues("trading.consecutive-empty-days-limit: 5")
                 .withPropertyValues("start-date: 2000-01-01T00:00:00+03:00")
-                .run(context -> assertContextStartupFailed(context,
-                        "trading.workDuration", "не должно равняться null")
-                );
-
+                .run(context -> assertContextStartupFailed(
+                        context,
+                        "trading.workDuration", "не должно равняться null"
+                ));
     }
 
     @Test
     void beanCreationFails_whenConsecutiveEmptyDaysLimitIsNull() {
-
         this.contextRunner
                 .withPropertyValues("trading.token: i identify myself as token")
                 .withPropertyValues("trading.work-start-time: 12:00:00+03:00")
                 .withPropertyValues("trading.work-duration: 480")
                 .withPropertyValues("start-date: 2000-01-01T00:00:00+03:00")
-                .run(context -> assertContextStartupFailed(context,
-                        "trading.consecutiveEmptyDaysLimit", "не должно равняться null")
-                );
-
+                .run(context -> assertContextStartupFailed(
+                        context,
+                        "trading.consecutiveEmptyDaysLimit", "не должно равняться null"
+                ));
     }
 
     @Test
     void beanCreationFails_whenStartDateIsNull() {
-
         this.contextRunner
                 .withPropertyValues("trading.token: i identify myself as token")
                 .withPropertyValues("trading.work-start-time: 12:00:00+03:00")
                 .withPropertyValues("trading.work-duration: 480")
                 .withPropertyValues("trading.consecutive-empty-days-limit: 5")
-                .run(context -> assertContextStartupFailed(context,
-                        "trading.startDate", "не должно равняться null")
-                );
-
+                .run(context -> assertContextStartupFailed(
+                        context,
+                        "trading.startDate", "не должно равняться null"
+                ));
     }
 
-    private void assertContextStartupFailed(AssertableApplicationContext context, String... messageSubstrings) {
-        Throwable startupFailure = context.getStartupFailure();
+    private void assertContextStartupFailed(
+            final AssertableApplicationContext context,
+            final String... messageSubstrings
+    ) {
+        final Throwable startupFailure = context.getStartupFailure();
 
         Assertions.assertNotNull(startupFailure);
 
-        String message = getBindValidationExceptionMessage(startupFailure);
+        final String message = getBindValidationExceptionMessage(startupFailure);
         for (String substring : messageSubstrings) {
             Assertions.assertTrue(message.contains(substring));
         }
     }
 
-    private String getBindValidationExceptionMessage(Throwable startupFailure) {
-        BindValidationException bindValidationException =
+    private String getBindValidationExceptionMessage(final Throwable startupFailure) {
+        final BindValidationException bindValidationException =
                 (BindValidationException) startupFailure.getCause().getCause();
         return bindValidationException.getMessage();
     }
