4c8fabe1a858b3ef0043c7c6c13f97631842c6f3
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 10:03:50 +0300

    used @ExtendWith in OrdersServiceImplUnitTest + refactor

--- src/test/java/ru/obukhov/trader/market/impl/OrdersServiceImplUnitTest.java
+++ src/test/java/ru/obukhov/trader/market/impl/OrdersServiceImplUnitTest.java
@@ -1,13 +1,12 @@
 package ru.obukhov.trader.market.impl;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.junit.runner.RunWith;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
-import ru.obukhov.trader.BaseMockedTest;
+import org.mockito.junit.jupiter.MockitoExtension;
 import ru.obukhov.trader.market.interfaces.MarketService;
 import ru.obukhov.trader.market.interfaces.TinkoffService;
 import ru.tinkoff.invest.openapi.model.rest.OperationType;
@@ -18,39 +17,32 @@ import ru.tinkoff.invest.openapi.model.rest.OrderType;
 import java.math.BigDecimal;
 import java.util.List;
 
-@RunWith(MockitoJUnitRunner.class)
-class OrdersServiceImplUnitTest extends BaseMockedTest {
-
-    private static final String FIGI = "figi";
-    private static final String TICKER = "ticker";
+@ExtendWith(MockitoExtension.class)
+class OrdersServiceImplUnitTest {
 
     @Mock
     private TinkoffService tinkoffService;
     @Mock
     private MarketService marketService;
 
+    @InjectMocks
     private OrdersServiceImpl service;
-
-    @BeforeEach
-    public void setUp() {
-        this.service = new OrdersServiceImpl(tinkoffService, marketService);
-    }
 
     @Test
     void getOrders_filtersOrdersByFigi() {
-        String ticker = TICKER;
-        String figi = FIGI;
+        final String ticker = "ticker";
+        final String figi = "figi";
 
-        mockFigi(ticker, figi);
-        mockOrders(List.of(
+        Mockito.when(marketService.getFigi(ticker)).thenReturn(figi);
+        mockOrders(
                 createOrder("order0", figi),
                 createOrder("order1", figi),
                 createOrder("order2", "figi3"),
                 createOrder("order3", "figi4"),
                 createOrder("order4", figi)
-        ));
+        );
 
-        List<Order> orders = service.getOrders(ticker);
+        final List<Order> orders = service.getOrders(ticker);
 
         Assertions.assertEquals(3, orders.size());
         Assertions.assertEquals("order0", orders.get(0).getOrderId());
@@ -58,12 +50,8 @@ class OrdersServiceImplUnitTest extends BaseMockedTest {
         Assertions.assertEquals("order4", orders.get(2).getOrderId());
     }
 
-    private void mockFigi(String ticker, String figi) {
-        Mockito.when(marketService.getFigi(ticker)).thenReturn(figi);
-    }
-
-    private void mockOrders(List<Order> orders) {
-        Mockito.when(tinkoffService.getOrders()).thenReturn(orders);
+    private void mockOrders(Order... orders) {
+        Mockito.when(tinkoffService.getOrders()).thenReturn(List.of(orders));
     }
 
     private Order createOrder(String id, String figi) {
