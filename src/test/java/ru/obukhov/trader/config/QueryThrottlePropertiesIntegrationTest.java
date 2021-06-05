8500c01791b2177d433de35ec8f79244d03df02b
auobukhov@gmail.com <auobukhov@gmail.com>
2021-06-05 11:19:04 +0300

    refactored QueryThrottlePropertiesIntegrationTest

--- src/test/java/ru/obukhov/trader/config/QueryThrottlePropertiesIntegrationTest.java
+++ src/test/java/ru/obukhov/trader/config/QueryThrottlePropertiesIntegrationTest.java
@@ -18,28 +18,27 @@ class QueryThrottlePropertiesIntegrationTest {
 
     @Test
     void beanCreating_whenPropertiesFilled() {
-
         contextRunner.run(context -> {
             Assertions.assertNull(context.getStartupFailure());
 
-            QueryThrottleProperties queryThrottleProperties = context.getBean(QueryThrottleProperties.class);
+            final QueryThrottleProperties queryThrottleProperties = context.getBean(QueryThrottleProperties.class);
 
             AssertUtils.assertEquals(60000L, queryThrottleProperties.getInterval());
 
-            List<UrlLimit> limits = queryThrottleProperties.getLimits();
+            final List<UrlLimit> limits = queryThrottleProperties.getLimits();
             Assertions.assertEquals(3, limits.size());
 
-            UrlLimit urlLimit0 = limits.get(0);
+            final UrlLimit urlLimit0 = limits.get(0);
             Assertions.assertEquals(1, urlLimit0.getSegments().size());
             Assertions.assertEquals("market", urlLimit0.getSegments().get(0));
             Assertions.assertEquals(120, urlLimit0.getLimit());
 
-            UrlLimit urlLimit1 = limits.get(1);
+            final UrlLimit urlLimit1 = limits.get(1);
             Assertions.assertEquals(1, urlLimit1.getSegments().size());
             Assertions.assertEquals("orders", urlLimit1.getSegments().get(0));
             Assertions.assertEquals(100, urlLimit1.getLimit());
 
-            UrlLimit urlLimit2 = limits.get(2);
+            final UrlLimit urlLimit2 = limits.get(2);
             Assertions.assertEquals(2, urlLimit2.getSegments().size());
             Assertions.assertEquals("orders", urlLimit2.getSegments().get(0));
             Assertions.assertEquals("limit-order", urlLimit2.getSegments().get(1));
@@ -56,27 +55,22 @@ class QueryThrottlePropertiesIntegrationTest {
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenIntervalIsNegative() {
-
         contextRunner.withPropertyValues("query.throttle.interval: -1")
                 .run(AssertUtils.createContextFailureAssertConsumer("interval must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenIntervalIsZero() {
-
         contextRunner.withPropertyValues("query.throttle.interval: 0")
                 .run(AssertUtils.createContextFailureAssertConsumer("interval must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenLimitsAreNull() {
-
         new ApplicationContextRunner()
                 .withUserConfiguration(EnableConfigurationPropertiesConfiguration.class)
                 .withPropertyValues("query.throttle.interval: 60000")
@@ -84,86 +78,70 @@ class QueryThrottlePropertiesIntegrationTest {
                 .withPropertyValues("query.throttle.attempts-count: 30")
                 .withPropertyValues("query.throttle.default-limit: 120")
                 .run(AssertUtils.createContextFailureAssertConsumer("limits must not be null"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenLimitsAreEmpty() {
-
         contextRunner.withPropertyValues("query.throttle.limits:")
                 .run(AssertUtils.createContextFailureAssertConsumer("limits must not be empty"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenRetryIntervalIsNegative() {
-
         contextRunner.withPropertyValues("query.throttle.retry-interval: -1")
                 .run(AssertUtils.createContextFailureAssertConsumer("retryInterval must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenRetryIntervalIsZero() {
-
         contextRunner.withPropertyValues("query.throttle.retry-interval: 0")
                 .run(AssertUtils.createContextFailureAssertConsumer("retryInterval must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenAttemptsCountIsNegative() {
-
         contextRunner.withPropertyValues("query.throttle.attempts-count: -1")
                 .run(AssertUtils.createContextFailureAssertConsumer("attemptsCount must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenAttemptsCountIsZero() {
-
         contextRunner.withPropertyValues("query.throttle.attempts-count: 0")
                 .run(AssertUtils.createContextFailureAssertConsumer("attemptsCount must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenSegmentsAreEmpty() {
-
         contextRunner.withPropertyValues("query.throttle.limits[0].segments=")
                 .withPropertyValues("query.throttle.limits[0].limit: 1")
                 .run(AssertUtils.createContextFailureAssertConsumer("segments must not be empty"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenUrlLimitIsNegative() {
-
         contextRunner.withPropertyValues("query.throttle.limits[0].segments[0]: market")
                 .withPropertyValues("query.throttle.limits[0].limits=-1")
                 .run(AssertUtils.createContextFailureAssertConsumer("limit must be positive"));
-
     }
 
     @Test
     @SuppressWarnings("java:S2699")
         // Sonar warning "Tests should include assertions"
     void beanCreationFails_whenUrlLimitIsZero() {
-
         contextRunner.withPropertyValues("query.throttle.interval: 60000")
                 .withPropertyValues("query.throttle.limits[0].segments[0]: market")
                 .withPropertyValues("query.throttle.limits[0].limits=0")
@@ -171,7 +149,6 @@ class QueryThrottlePropertiesIntegrationTest {
                 .withPropertyValues("query.throttle.attempts-count: 1")
                 .withPropertyValues("query.throttle.default-limit: 1")
                 .run(AssertUtils.createContextFailureAssertConsumer("limit must be positive"));
-
     }
 
     @EnableConfigurationProperties(QueryThrottleProperties.class)
