b3c6c7e77f22caeef500dd7b53814d318e2fc798
auobukhov@gmail.com <auobukhov@gmail.com>
2021-07-17 10:02:45 +0300

    removed default http statuses from swagger doc

--- src/main/java/ru/obukhov/trader/config/SpringFoxConfig.java
+++ src/main/java/ru/obukhov/trader/config/SpringFoxConfig.java
@@ -15,6 +15,7 @@ public class SpringFoxConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .useDefaultResponseMessages(false)
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
