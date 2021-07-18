96cf23e2e8e3165ede3e122afba1fcdf7c44f3ae
auobukhov@gmail.com <auobukhov@gmail.com>
2021-07-18 14:34:40 +0300

    updated AssertUtils#assertListsAreEqual for multidimensional lists

--- src/test/java/ru/obukhov/trader/test/utils/AssertUtils.java
+++ src/test/java/ru/obukhov/trader/test/utils/AssertUtils.java
@@ -144,9 +144,12 @@ public class AssertUtils {
         for (int i = 0; i < expected.size(); i++) {
             final Object expectedValue = expected.get(i);
             final Object actualValue = actual.get(i);
-            if (!Objects.equals(expectedValue, actualValue)) {
-                final String message = String.format("expected: <%s> at position <%s> but was: <%s>",
-                        expectedValue, i, actualValue);
+            if (expectedValue instanceof List && actualValue instanceof List) {
+                assertListsAreEqual((List<?>) expectedValue, (List<?>) actualValue);
+            } else if (!Objects.equals(expectedValue, actualValue)) {
+                final String message = String.format(
+                        "expected: <%s> at position <%s> but was: <%s>", expectedValue, i, actualValue
+                );
                 Assertions.fail(message);
             }
         }
