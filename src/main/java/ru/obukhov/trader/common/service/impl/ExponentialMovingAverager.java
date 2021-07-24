b1a2c6ada42dbb4ff45a16a5d2dde70f02941f74
auobukhov@gmail.com <auobukhov@gmail.com>
2021-07-24 14:18:10 +0300

    refactored averagers

--- src/main/java/ru/obukhov/trader/common/service/impl/ExponentialMovingAverager.java
+++ src/main/java/ru/obukhov/trader/common/service/impl/ExponentialMovingAverager.java
@@ -2,6 +2,7 @@ package ru.obukhov.trader.common.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import ru.obukhov.trader.common.service.interfaces.MovingAverager;
 import ru.obukhov.trader.common.util.DecimalUtils;
 
 import java.math.BigDecimal;
@@ -14,19 +15,30 @@ import java.util.stream.Collectors;
  * Class with methods for calculation of exponentially weighted moving averages
  */
 @Service
-public class ExponentialMovingAverager extends SimpleMovingAverager {
+public class ExponentialMovingAverager implements MovingAverager {
+
+    @Override
+    public <T> List<BigDecimal> getAverages(
+            final List<T> elements,
+            final Function<T, BigDecimal> valueExtractor,
+            final int window,
+            final int order
+    ) {
+        final List<BigDecimal> values = elements.stream().map(valueExtractor).collect(Collectors.toList());
+        return getAverages(values, window, order);
+    }
 
     @Override
     public List<BigDecimal> getAverages(final List<BigDecimal> values, final int window, final int order) {
         Assert.isTrue(order > 0, "order must be positive");
         Assert.isTrue(window > 0, "window must be positive");
 
-        final double weightDecrease = 2.0 / (window + 1);
-        final double revertedWeightDecrease = 1 - weightDecrease;
-        final List<BigDecimal> averages = new ArrayList<>(values);
+        List<BigDecimal> averages = new ArrayList<>(values);
         if (averages.isEmpty()) {
             return averages;
         }
+        final double weightDecrease = 2.0 / (window + 1);
+        final double revertedWeightDecrease = 1.0 - weightDecrease;
 
         for (int i = 0; i < order; i++) {
             updateExponentialMovingAverages(averages, weightDecrease, revertedWeightDecrease);
--- src/main/java/ru/obukhov/trader/common/service/impl/LinearMovingAverager.java
+++ src/main/java/ru/obukhov/trader/common/service/impl/LinearMovingAverager.java
@@ -2,17 +2,57 @@ package ru.obukhov.trader.common.service.impl;
 
 import org.springframework.stereotype.Service;
 import org.springframework.util.Assert;
+import ru.obukhov.trader.common.service.interfaces.MovingAverager;
 import ru.obukhov.trader.common.util.DecimalUtils;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 /**
  * Class with methods for calculation of linear weighted moving averages
  */
 @Service
-public class LinearMovingAverager extends SimpleMovingAverager {
+public class LinearMovingAverager implements MovingAverager {
+
+    @Override
+    public <T> List<BigDecimal> getAverages(
+            final List<T> elements,
+            final Function<T, BigDecimal> valueExtractor,
+            final int window,
+            final int order
+    ) {
+        final List<BigDecimal> values = elements.stream().map(valueExtractor).collect(Collectors.toList());
+        return getAverages(values, window, order);
+    }
+
+    @Override
+    public List<BigDecimal> getAverages(
+            final List<BigDecimal> values,
+            final int window,
+            final int order
+    ) {
+        Assert.isTrue(order > 0, "order must be positive");
+
+        List<BigDecimal> averages = new ArrayList<>(values);
+        for (int i = 0; i < order; i++) {
+            averages = getAverages(averages, window);
+        }
+
+        return averages;
+    }
+
+    @Override
+    public <T> List<BigDecimal> getAverages(
+            final List<T> elements,
+            final Function<T, BigDecimal> valueExtractor,
+            final int window
+    ) {
+        final List<BigDecimal> values = elements.stream().map(valueExtractor).collect(Collectors.toList());
+        return getAverages(values, window);
+    }
 
     @Override
     public List<BigDecimal> getAverages(final List<BigDecimal> values, final int window) {
