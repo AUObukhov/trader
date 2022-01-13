6e9a60cd2dec4441969f1774edc346442dd20e62
Obukhov Artem <auobukhov@gmail.com>
2022-01-13 12:33:39 +0300

    added javadocs

--- src/main/java/ru/obukhov/trader/market/impl/PortfolioService.java
+++ src/main/java/ru/obukhov/trader/market/impl/PortfolioService.java
@@ -9,7 +9,11 @@ import ru.tinkoff.invest.openapi.model.rest.CurrencyPosition;
 
 import java.math.BigDecimal;
 import java.util.List;
+import java.util.NoSuchElementException;
 
+/**
+ * Service to get information about customer portfolio
+ */
 @RequiredArgsConstructor
 public class PortfolioService {
 
@@ -19,6 +23,9 @@ public class PortfolioService {
         return tinkoffService.getPortfolioPositions(brokerAccountId);
     }
 
+    /**
+     * @return position with given {@code ticker} at given {@code brokerAccountId} or null, if such position does not exist
+     */
     public PortfolioPosition getPosition(@Nullable final String brokerAccountId, final String ticker) {
         final List<PortfolioPosition> allPositions = getPositions(brokerAccountId);
         return allPositions.stream()
@@ -27,6 +34,11 @@ public class PortfolioService {
                 .orElse(null);
     }
 
+    /**
+     * @return available balance of given {@code currency} at given {@code brokerAccountId}
+     * @throws NoSuchElementException if given {@code currency} not found.
+     *                                Currencies currently available: {@link Currency.EUR}, {@link Currency.USD}, {@link Currency.RUB}
+     */
     public BigDecimal getAvailableBalance(@Nullable final String brokerAccountId, final Currency currency) {
         return getCurrencies(brokerAccountId).stream()
                 .filter(portfolioCurrency -> portfolioCurrency.getCurrency() == currency)
@@ -41,6 +53,9 @@ public class PortfolioService {
                 : currency.getBalance().subtract(currency.getBlocked());
     }
 
+    /**
+     * @return list of currencies balances at given {@code brokerAccountId}
+     */
     public List<CurrencyPosition> getCurrencies(@Nullable final String brokerAccountId) {
         return tinkoffService.getPortfolioCurrencies(brokerAccountId);
     }
