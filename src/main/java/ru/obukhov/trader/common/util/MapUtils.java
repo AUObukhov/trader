aafab6a80a3e3882cfb9e3e1a9d40a498a9b6d6c
Obukhov Artem <auobukhov@gmail.com>
2022-09-14 18:32:37 +0300

    refactored MapUtils

--- src/main/java/ru/obukhov/trader/common/util/MapUtils.java
+++ src/main/java/ru/obukhov/trader/common/util/MapUtils.java
@@ -2,6 +2,7 @@ package ru.obukhov.trader.common.util;
 
 import lombok.experimental.UtilityClass;
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.util.Assert;
 
 import java.util.Map;
 
@@ -10,25 +11,19 @@ public class MapUtils {
 
     public static String getRequiredString(final Map<String, Object> map, final String key) {
         final String value = (String) map.get(key);
-        if (value == null) {
-            throw new IllegalArgumentException("\"" + key + "\" is mandatory");
-        }
+        Assert.notNull(value, () -> "\"" + key + "\" is mandatory");
         return value;
     }
 
     public static String getNotBlankString(final Map<String, Object> map, final String key) {
         final String value = (String) map.get(key);
-        if (StringUtils.isBlank(value)) {
-            throw new IllegalArgumentException("\"" + key + "\" must be not blank");
-        }
+        Assert.isTrue(StringUtils.isNotBlank(value), () -> "\"" + key + "\" must be not blank");
         return value;
     }
 
     public static Integer getRequiredInteger(final Map<String, Object> map, final String key) {
         final Integer value = (Integer) map.get(key);
-        if (value == null) {
-            throw new IllegalArgumentException("\"" + key + "\" is mandatory");
-        }
+        Assert.notNull(value, () -> "\"" + key + "\" is mandatory");
         return value;
     }
 
