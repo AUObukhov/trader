6c080a2781c689449dfe51181d3231559ac7a9f5
Obukhov Artem <auobukhov@gmail.com>
2022-10-09 09:43:05 +0300

    added javadocs

--- src/main/java/ru/obukhov/trader/market/impl/ExtInstrumentsService.java
+++ src/main/java/ru/obukhov/trader/market/impl/ExtInstrumentsService.java
@@ -35,6 +35,10 @@ public class ExtInstrumentsService implements ApplicationContextAware {
     private final InstrumentsService instrumentsService;
     private ExtInstrumentsService self;
 
+    /**
+     * @return FIGI corresponding to given {@code ticker}
+     * @throws IllegalArgumentException when given {@code ticker} has no corresponding FIGIes or has more than one corresponding FIGI
+     */
     @Cacheable(value = "singleFigiByTicker", sync = true)
     public String getSingleFigiByTicker(final String ticker) {
         final List<String> figies = self.getFigiesByTicker(ticker);
@@ -42,6 +46,9 @@ public class ExtInstrumentsService implements ApplicationContextAware {
         return figies.get(0);
     }
 
+    /**
+     * @return FIGIes corresponding to given {@code ticker}
+     */
     @Cacheable(value = "figiesByTicker", sync = true)
     public List<String> getFigiesByTicker(final String ticker) {
         return instrumentsService.getAssetsSync().stream()
@@ -51,6 +58,10 @@ public class ExtInstrumentsService implements ApplicationContextAware {
                 .toList();
     }
 
+    /**
+     * @return ticker corresponding to given {@code figi}
+     * @throws IllegalArgumentException when instrument with given {@code figi} does not exist
+     */
     @Cacheable(value = "tickerByFigi", sync = true)
     public String getTickerByFigi(final String figi) {
         final Instrument instrument = instrumentsService.getInstrumentByFigiSync(figi);
@@ -58,6 +69,9 @@ public class ExtInstrumentsService implements ApplicationContextAware {
         return instrument.getTicker();
     }
 
+    /**
+     * @return list of {@link Share} corresponding to given {@code ticker}
+     */
     public List<Share> getShares(final String ticker) {
         return instrumentsService.getAllSharesSync().stream()
                 .filter(share -> ticker.equalsIgnoreCase(share.getTicker()))
@@ -65,12 +79,19 @@ public class ExtInstrumentsService implements ApplicationContextAware {
                 .toList();
     }
 
+    /**
+     * @return {@link Share} corresponding to given {@code ticker}
+     * @throws IllegalArgumentException when given {@code ticker} has no corresponding share or has more than one corresponding share
+     */
     public Share getSingleShare(final String ticker) {
         final List<Share> shares = getShares(ticker);
         Assert.isTrue(shares.size() == 1, () -> getInstrumentsCountErrorMessage("share", ticker, shares.size()));
         return shares.get(0);
     }
 
+    /**
+     * @return list of {@link Etf} corresponding to given {@code ticker}
+     */
     public List<Etf> getEtfs(final String ticker) {
         return instrumentsService.getAllEtfsSync().stream()
                 .filter(etf -> ticker.equalsIgnoreCase(etf.getTicker()))
@@ -78,12 +99,19 @@ public class ExtInstrumentsService implements ApplicationContextAware {
                 .toList();
     }
 
+    /**
+     * @return {@link Etf} corresponding to given {@code ticker}
+     * @throws IllegalArgumentException when given {@code ticker} has no corresponding etf or has more than one corresponding etf
+     */
     public Etf getSingleEtf(final String ticker) {
         final List<Etf> etfs = getEtfs(ticker);
         Assert.isTrue(etfs.size() == 1, () -> getInstrumentsCountErrorMessage("etf", ticker, etfs.size()));
         return etfs.get(0);
     }
 
+    /**
+     * @return list of {@link TradingDay} with given {@code interval} corresponding to given {@code exchange}
+     */
     public List<TradingDay> getTradingSchedule(final Exchange exchange, final Interval interval) {
         final Instant fromInstant = interval.getFrom().toInstant();
         final Instant toInstant = interval.getTo().toInstant();
@@ -94,6 +122,9 @@ public class ExtInstrumentsService implements ApplicationContextAware {
                 .toList();
     }
 
+    /**
+     * @return list of {@link TradingSchedule} with given {@code interval}. Each schedule corresponds to some exchange
+     */
     public List<TradingSchedule> getTradingSchedules(final Interval interval) {
         return instrumentsService.getTradingSchedulesSync(interval.getFrom().toInstant(), interval.getTo().toInstant())
                 .stream()
