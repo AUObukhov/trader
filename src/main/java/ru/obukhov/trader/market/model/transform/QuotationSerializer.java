fbf27941e4bf9bd82dd6b86700737820236db05e
Obukhov Artem <auobukhov@gmail.com>
2023-08-22 09:45:08 +0300

    made Quotation serialization simple

--- src/main/java/ru/obukhov/trader/market/model/transform/QuotationSerializer.java
+++ src/main/java/ru/obukhov/trader/market/model/transform/QuotationSerializer.java
@@ -3,6 +3,7 @@ package ru.obukhov.trader.market.model.transform;
 import com.fasterxml.jackson.core.JsonGenerator;
 import com.fasterxml.jackson.databind.JsonSerializer;
 import com.fasterxml.jackson.databind.SerializerProvider;
+import ru.obukhov.trader.common.util.QuotationUtils;
 import ru.tinkoff.piapi.contract.v1.Quotation;
 
 import java.io.IOException;
@@ -17,12 +18,7 @@ public class QuotationSerializer extends JsonSerializer<Quotation> {
     @Override
     public void serialize(final Quotation value, final JsonGenerator jgen, final SerializerProvider provider)
             throws IOException {
-        jgen.writeStartObject();
-
-        jgen.writeNumberField("units", value.getUnits());
-        jgen.writeNumberField("nano", value.getNano());
-
-        jgen.writeEndObject();
+        jgen.writeRawValue(QuotationUtils.toPrettyString(value));
     }
 
 }
--- src/test/java/ru/obukhov/trader/market/model/transform/QuotationSerializerUnitTest.java
+++ src/test/java/ru/obukhov/trader/market/model/transform/QuotationSerializerUnitTest.java
@@ -1,6 +1,8 @@
 package ru.obukhov.trader.market.model.transform;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+import ru.obukhov.trader.common.util.QuotationUtils;
 import ru.tinkoff.piapi.contract.v1.Quotation;
 
 import java.io.IOException;
@@ -9,13 +11,23 @@ class QuotationSerializerUnitTest extends SerializerAbstractUnitTest<Quotation> 
 
     private final QuotationSerializer quotationSerializer = new QuotationSerializer();
 
-    @Test
-    void test() throws IOException {
-        final Quotation quotation = Quotation.newBuilder()
-                .setUnits(-10)
-                .setNano(40000000)
-                .build();
-        final String expectedResult = "{\"units\":-10,\"nano\":40000000}";
+    @ParameterizedTest
+    @CsvSource(value = {
+            "0, 0, 0",
+            "1000, 0, 1000",
+            "0, 2000, 0.000002",
+            "3000, 4000, 3000.000004",
+            "5000, 100000000, 5000.1",
+            "6000, 7, 6000.000000007",
+            "-8000, 0,-8000",
+            "0, -9000, -0.000009",
+            "-1100, -1200, -1100.0000012",
+            "-1300, -140000000, -1300.14",
+            "-1500, -1, -1500.000000001",
+    })
+    void test(final long units, final int nano, final String expectedResult) throws IOException {
+        final Quotation quotation = QuotationUtils.newQuotation(units, nano);
+
         test(quotationSerializer, quotation, expectedResult);
     }
 
