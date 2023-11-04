f5da1235a31a2f66cabac7125c22b60d83e15a70
Obukhov Artem <auobukhov@gmail.com>
2023-11-04 09:05:04 +0300

    added constructor with multiple instrument ids to InstrumentNotFoundException

--- src/main/java/ru/obukhov/trader/common/exception/InstrumentNotFoundException.java
+++ src/main/java/ru/obukhov/trader/common/exception/InstrumentNotFoundException.java
@@ -1,9 +1,15 @@
 package ru.obukhov.trader.common.exception;
 
+import java.util.Collection;
+
 public class InstrumentNotFoundException extends RuntimeException {
 
     public InstrumentNotFoundException(final String instrumentId) {
         super("Instrument not found for id " + instrumentId);
+    }
+
+    public InstrumentNotFoundException(final Collection<String> instrumentIds) {
+        super("Instruments not found for ids " + instrumentIds);
     }
 
 }
--- src/test/java/ru/obukhov/trader/common/exception/InstrumentNotFoundExceptionUnitTest.java
+++ src/test/java/ru/obukhov/trader/common/exception/InstrumentNotFoundExceptionUnitTest.java
@@ -4,14 +4,26 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import ru.obukhov.trader.test.utils.model.instrument.TestInstruments;
 
+import java.util.List;
+
 class InstrumentNotFoundExceptionUnitTest {
 
     @Test
-    void constructorSetsMessage() {
+    void constructorSetsMessage_whenSingleInstrumentId() {
         final String instrumentId = TestInstruments.APPLE.instrument().figi();
         final InstrumentNotFoundException exception = new InstrumentNotFoundException(instrumentId);
 
         final String expectedMessage = "Instrument not found for id " + instrumentId;
+        Assertions.assertEquals(expectedMessage, exception.getMessage());
+    }
+
+    @Test
+    void constructorSetsMessage_whenMultipleInstrumentIds() {
+        final String instrumentId1 = TestInstruments.APPLE.instrument().figi();
+        final String instrumentId2 = TestInstruments.SBER.instrument().figi();
+        final InstrumentNotFoundException exception = new InstrumentNotFoundException(List.of(instrumentId1, instrumentId2));
+
+        final String expectedMessage = "Instruments not found for ids [" + instrumentId1 + ", " + instrumentId2 + "]";
         Assertions.assertEquals(expectedMessage, exception.getMessage());
     }
 
