147f79069e9c007e1243468e4aca46358b26cb18
Obukhov Artem <auobukhov@gmail.com>
2024-05-29 12:04:49 +0300

    updated java version

--- src/main/java/ru/obukhov/trader/Application.java
+++ src/main/java/ru/obukhov/trader/Application.java
@@ -2,12 +2,13 @@ package ru.obukhov.trader;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-@SpringBootApplication(scanBasePackages = {"ru.tinkoff.invest.openapi.okhttp", "ru.obukhov.trader"})
+@SpringBootApplication(scanBasePackages = {"ru.tinkoff.invest.openapi.okhttp", "ru.obukhov.trader"}, exclude = DataSourceAutoConfiguration.class)
 @EnableCaching
 @EnableScheduling
 @EnableAspectJAutoProxy
