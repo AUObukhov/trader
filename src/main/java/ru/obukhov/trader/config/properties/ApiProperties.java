0df4995620f0563bb853df2af6cfcdc0b6149cda
Obukhov Artem <auobukhov@gmail.com>
2024-08-18 11:04:08 +0600

    removed unused properties

--- src/main/java/ru/obukhov/trader/config/properties/ApiProperties.java
+++ src/main/java/ru/obukhov/trader/config/properties/ApiProperties.java
@@ -1,24 +1,16 @@
 package ru.obukhov.trader.config.properties;
 
-import jakarta.validation.constraints.NotNull;
 import org.apache.commons.lang3.ObjectUtils;
-import org.jetbrains.annotations.Nullable;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.bind.ConstructorBinding;
 import org.springframework.validation.annotation.Validated;
 
 @ConfigurationProperties(prefix = "ru.tinkoff.invest.openapi")
 @Validated
-public record ApiProperties(
-        @NotNull(message = "host is mandatory") String host,
-        @Nullable Integer port,
-        Long throttlingInterval
-) {
+public record ApiProperties(Long throttlingInterval) {
 
     @ConstructorBinding
-    public ApiProperties(final String host, final Integer port, final Long throttlingInterval) {
-        this.host = host;
-        this.port = port;
+    public ApiProperties(final Long throttlingInterval) {
         this.throttlingInterval = ObjectUtils.defaultIfNull(throttlingInterval, 60000L);
     }
 
--- src/test/java/ru/obukhov/trader/config/properties/ApiPropertiesContextTest.java
+++ src/test/java/ru/obukhov/trader/config/properties/ApiPropertiesContextTest.java
@@ -5,7 +5,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.context.ConfigDataApplicationContextInitializer;
 import org.springframework.boot.test.context.runner.ApplicationContextRunner;
-import ru.obukhov.trader.test.utils.AssertUtils;
 
 class ApiPropertiesContextTest {
 
@@ -22,28 +21,19 @@ class ApiPropertiesContextTest {
 
                     final ApiProperties apiProperties = context.getBean(ApiProperties.class);
 
-                    Assertions.assertEquals("http://localhost", apiProperties.host());
-                    Assertions.assertEquals(8081, apiProperties.port());
                     Assertions.assertEquals(1000, apiProperties.throttlingInterval());
                 });
     }
 
     @Test
-    void beanCreationFails_whenHostIsNull() {
-        this.contextRunner.run(context -> AssertUtils.assertContextStartupFailed(context, "host is mandatory"));
-    }
-
-    @Test
     void throttlingIntervalInitializedWithDefaultValue_whenNull() {
-        contextRunner
-                .withPropertyValues("ru.tinkoff.invest.openapi.host: http://localhost")
-                .run(context -> {
-                    Assertions.assertNull(context.getStartupFailure());
+        contextRunner.run(context -> {
+            Assertions.assertNull(context.getStartupFailure());
 
-                    final ApiProperties apiProperties = context.getBean(ApiProperties.class);
+            final ApiProperties apiProperties = context.getBean(ApiProperties.class);
 
-                    Assertions.assertEquals(60000, apiProperties.throttlingInterval());
-                });
+            Assertions.assertEquals(60000, apiProperties.throttlingInterval());
+        });
     }
 
     @EnableConfigurationProperties(ApiProperties.class)
